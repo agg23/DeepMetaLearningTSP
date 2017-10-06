0b40e9c9ecda34dffda3dee5cfe1a8e6b44abe67
Adam Gastineau <adam@appcannonsoftware.com>
2017-10-06 09:42:17 -0400

    Added universal cost calculation

--- Solver/utilities.py
+++ Solver/utilities.py
@@ -10,3 +10,22 @@ def generateInitialSolution(tsp):
 		solution[randomIndex] = temp
 
 	return solution
+
+def calculateCost(solution, tsp):
+	cost = 0
+
+	if solution.shape[0] < 2:
+		return cost
+
+	previous = null
+	current = solution[0]
+
+	for i in range(1, solution.shape[0]):
+		temp = solution[i]
+
+		cost += tsp.getCost(current, temp)
+
+		previous = current
+		current = temp
+
+	return cost
