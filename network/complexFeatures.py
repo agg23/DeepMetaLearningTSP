08dda394d5244feb5c69dcf55c4a808c98397df4
Adam Gastineau <adam@appcannonsoftware.com>
2017-10-30 10:57:52 -0400

    Added complex features

---
+++ network/complexFeatures.py
@@ -0,0 +1,53 @@
+import sys
+import numpy
+
+from .features import dijkstra
+
+# May be different than Kanda's implementation
+def averageShortestPathGeodesicDistance(tsp):
+	costSum = 0
+	edgeCount = 0
+	for city in range(tsp.getSize()):
+		visited, paths = dijkstra(tsp, city)
+
+		for innerCity in range(tsp.getSize()):
+			if city == innerCity:
+				continue
+
+			_destination = paths[innerCity]
+
+			while _destination != city:
+				_destination = paths[_destination]
+
+			shortestPathCost = visited[innerCity]
+			costSum += shortestPathCost
+			edgeCount += 1
+
+	return costSum/edgeCount
+
+def averageGeodesicDistance(tsp):
+	return numpy.mean(tsp.costs[numpy.nonzero(tsp.costs)])
+
+def globalEfficiency(tsp):
+	return numpy.mean(1/tsp.costs[numpy.nonzero(tsp.costs)])
+
+def harmonicMeanGeodesicDistance(tsp):
+	# Number of edges minus diagonal
+	return (tsp.getSize() * (tsp.getSize() - 1)) * numpy.sum(1/tsp.costs[numpy.nonzero(tsp.costs)])
+
+def networkVulnerability(cache, tsp):
+	ge = cache.globalEfficiency
+	maxVulnerability = 0
+	for city in range(tsp.getSize()):
+		vulnerability = networkVulnerabilityAt(city, tsp, ge)
+
+		if vulnerability > maxVulnerability:
+			maxVulnerability = vulnerability
+
+	return maxVulnerability
+
+def networkVulnerabilityAt(i, tsp, ge):
+	# Since assuming fully connected, first term will always be 1
+	return 1/(tsp.getSize() - (tsp.getSize() - 1)) * ge - numpy.mean(1/tsp.costs[i,numpy.nonzero(tsp.costs[i,])])
+
+# CCT, ACC, CCW, NCC, MDV, CED, EDD, TE, PCV, ER, CCA omitted due to irrelevance to fully connected graphs
--- network/features.py
+++ network/features.py
@@ -1,5 +1,7 @@
 # Utilities
 
+from collections import defaultdict, deque
+
 def vertexCost(tsp, index):
 	sumValue = 0
 
@@ -17,3 +19,67 @@ def buildVertexCosts(tsp):
 		costs[city] = vertexCost(tsp, city)
 
 	return costs
+
+
+
+class Graph(object):
+    def __init__(self):
+        self.nodes = set()
+        self.edges = defaultdict(list)
+        self.distances = {}
+
+    def add_node(self, value):
+        self.nodes.add(value)
+
+    def add_edge(self, from_node, to_node, distance):
+        self.edges[from_node].append(to_node)
+        self.edges[to_node].append(from_node)
+        self.distances[(from_node, to_node)] = distance
+
+
+# From Gist https://gist.github.com/mdsrosa/c71339cb23bc51e711d8
+def dijkstra(tsp, initial):
+    visited = {initial: 0}
+    path = {}
+
+    nodes = set(range(tsp.getSize()))
+
+    while nodes:
+        min_node = None
+        for node in nodes:
+            if node in visited:
+                if min_node is None:
+                    min_node = node
+                elif visited[node] < visited[min_node]:
+                    min_node = node
+        if min_node is None:
+            break
+
+        nodes.remove(min_node)
+        current_weight = visited[min_node]
+
+        for second_node in range(tsp.getSize()):
+            try:
+                weight = current_weight + tsp.getCost(min_node, second_node)
+            except:
+                continue
+            if second_node not in visited or weight < visited[second_node]:
+                visited[second_node] = weight
+                path[second_node] = min_node
+
+    return visited, path
+
+
+def dijkstraShortestPath(tsp, origin, destination):
+    visited, paths = dijkstra(tsp, origin)
+    full_path = deque()
+    _destination = paths[destination]
+
+    while _destination != origin:
+        full_path.appendleft(_destination)
+        _destination = paths[_destination]
+
+    full_path.appendleft(origin)
+    full_path.append(destination)
+
+    return visited[destination], list(full_path)
