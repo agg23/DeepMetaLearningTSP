73b1336b0dcb6ac9703e33cbc5903f55dc29a5be
Adam Gastineau <adam@appcannonsoftware.com>
2017-11-15 09:33:44 -0500

    Added generator for asymmetric instances

--- solver/generator.py
+++ solver/generator.py
@@ -17,15 +17,31 @@ class Generator(object):
 
 		tsp = SymmetricTSP(n)
 
-		for i in range(0, n):
+		for i in range(n):
 			p1 = points[i]
 
-			for j in range(0, n):
+			for j in range(n):
 				if i < j:
 					p2 = points[j]
 					cost = euclidianDistance3D(p1[0], p1[1], p1[2], p2[0], p2[1], p2[2])
 					tsp.setCost(i, j, cost)
 					tsp.setCost(j, i, cost)
+
+			# Add diagonal
+			tsp.setCost(i, i, 0)
+			tsp.setAdjacent(i, i, False)
+
+		return tsp
+
+	def generateAsymmetric(self, n):
+		tsp = AsymmetricTSP(n)
+
+		# Generate costs bounded between 0 and 1000 for each
+		for i in range(n):
+			for j in range(n):
+				cost = random.uniform(0, 1000)
+
+				tsp.setCost(i, j, cost)
 
 			# Add diagonal
 			tsp.setCost(i, i, 0)
